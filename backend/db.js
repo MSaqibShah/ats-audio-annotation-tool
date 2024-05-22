481c1d8fc30883586d32333940f6ba5f07d84210
S SHAH <msaqibshah001@gmail.com>
2024-05-22 15:28:28 +0530

    minor fix

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 const config = require("./config"); // Adjust the path as necessary to import your config
-console.log(config);
 // Function to connect to MongoDB
 const connectDB = async () => {
   const config_db = {
@@ -14,10 +13,9 @@ const connectDB = async () => {
     config_db.pass = process.env.MONGO_DB_PASS_PROD;
   }
 
-  console.log(config_db);
   try {
-    // await mongoose.connect(config.MONGO_DB_URI, config_db);
-    await mongoose.connect("mongodb://127.0.0.1:27017/ats_tool", config_db);
+    await mongoose.connect(config.MONGO_DB_URI, config_db);
+    // await mongoose.connect("mongodb://127.0.0.1:27017/ats_tool", config_db);
     console.log("Connected to MongoDB At:", config.MONGO_DB_URI);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
